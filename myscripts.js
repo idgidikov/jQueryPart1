50e67c9674ec12356375ded0c4e991017b48b409
idgidikov <idgidikov@abv.bg>
2021-05-09 17:19:05 +0300

    first commit

---
+++ myscripts.js
@@ -0,0 +1,13 @@
+//$("h1").click(function(){})
+
+//$("h1").on("mouseenter",function(){
+//  $(this).toggleClass("turnBlue");
+// })
+
+// $("input").eq(1).on("click",function(){
+//   $(".container").fadeOut(3000)
+// })
+
+$("input").eq(1).on("click",function(){
+  $(".container").slideUp(3000)
+})
